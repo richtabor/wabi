3190305a66ea1d79652edad00bd63d93e06ef272
Rich Tabor <hi@richtabor.com>
2022-11-22 15:23:28 -0500

    Clean template parts

--- inc/patterns/footer-default.php
+++ inc/patterns/footer-default.php
@@ -7,28 +7,18 @@ return array(
 	'categories' => array( 'wabi', 'footer' ),
 	'blockTypes' => array( 'core/template-part/footer' ),
 	'content'    => '
-		<!-- wp:group {"align":"full", "layout":{"type":"flex","justifyContent":"space-between"}} -->
-		<div class="wp-block-group alignfull">
-			<!-- wp:group {"layout":{"type":"flex","allowOrientation":false}} -->
-			<div class="wp-block-group">
-				<!-- wp:site-title /-->
+		<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|30","bottom":"var:preset|spacing|20","left":"var:preset|spacing|30"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+		<div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--30)"><!-- wp:group {"layout":{"type":"flex","allowOrientation":false}} -->
+		<div class="wp-block-group"><!-- wp:site-title /-->
 
-				<!-- wp:paragraph {"style":{"typography":{"fontSize":"0.9rem"}},"textColor":"foreground-alt"} -->
+		<!-- wp:paragraph {"style":{"typography":{"fontSize":"0.9rem"}},"textColor":"foreground-alt"} -->
+		<p class="has-foreground-alt-color has-text-color" style="font-size:0.9rem">
+					Proudly powered by <a href="https://wordpress.org" rel="nofollow">WordPress</a></p>
+		<!-- /wp:paragraph --></div>
+		<!-- /wp:group -->
 
-				<p class="has-foreground-alt-color has-text-color" style="font-size:0.9rem">' .
-				sprintf(
-					/* Translators: WordPress link. */
-					esc_html__( 'Proudly powered by %s', 'wabi' ),
-					'<a href="' . esc_url( __( 'https://wordpress.org', 'wabi' ) ) . '" rel="nofollow">WordPress</a>'
-				) . '</p>
-				<!-- /wp:paragraph -->
-			</div>
-			<!-- /wp:group -->
-
-			<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"},"style":{"spacing":{"blockGap":"1.5rem"}}} -->
-				<!-- wp:page-list {"isNavigationChild":true,"showSubmenuIcon":true,"openSubmenusOnClick":false} /-->
-			<!-- /wp:navigation -->
-
-		</div>
+		<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"},"style":{"spacing":{"blockGap":"1.5rem"}}} -->
+		<!-- wp:page-list /-->
+		<!-- /wp:navigation --></div>
 		<!-- /wp:group -->',
 );
--- inc/patterns/footer-w-posts.php
+++ inc/patterns/footer-w-posts.php
@@ -23,25 +23,7 @@ return array(
 		<!-- wp:spacer -->
 		<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
 		<!-- /wp:spacer -->
-		<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var(--wp--custom--spacing--large, 8rem)","bottom":"var(--wp--custom--spacing--small, 1.25rem)"}}},"layout":{"inherit":false}} -->
-		<div class="wp-block-group alignfull" style="padding-top:var(--wp--custom--spacing--large, 8rem);padding-bottom:var(--wp--custom--spacing--small, 1.25rem)"><!-- wp:group {"align":"full","layout":{"type":"flex","justifyContent":"space-between"}} -->
-		<div class="wp-block-group alignfull"><!-- wp:group {"layout":{"type":"flex","allowOrientation":false}} -->
-		<div class="wp-block-group"><!-- wp:site-title /-->
 
-		<!-- wp:paragraph {"style":{"typography":{"fontSize":"0.9rem"}},"textColor":"foreground-alt"} -->
-		<p class="has-foreground-alt-color has-text-color" style="font-size:0.9rem">' .
-			sprintf(
-				/* Translators: WordPress link. */
-				esc_html__( 'Proudly powered by %s', 'wabi' ),
-				'<a href="' . esc_url( __( 'https://wordpress.org', 'wabi' ) ) . '" rel="nofollow">WordPress</a>'
-			) . '</p>
-		<!-- /wp:paragraph --></div>
-		<!-- /wp:group -->
-
-		<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"},"style":{"spacing":{"blockGap":"1.5rem"}}} -->
-		<!-- wp:page-list /-->
-		<!-- /wp:navigation --></div>
-		<!-- /wp:group --></div>
-		<!-- /wp:group -->
+		<!-- wp:pattern {"slug":"wabi/footer-default"} /-->
 	',
 );
