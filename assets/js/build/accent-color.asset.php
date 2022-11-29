b99239d4d6e01e4b122b6786389c9a0968d1919d
Rich Tabor <hi@richtabor.com>
2022-11-29 10:56:14 -0500

    Clean up

--- assets/js/build/accent-color.asset.php
+++ assets/js/build/accent-color.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-compose', 'wp-data', 'wp-editPost', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => '3be8ff3345e22afce93476d83072701e');
+<?php return array('dependencies' => array('wp-components', 'wp-compose', 'wp-data', 'wp-editPost', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => 'ddb8993d834d481a931d');
